61338bdddb9d4a8aaceb077273f2d1089a7a41a3
endorf8n <128092594+endorf8n@users.noreply.github.com>
2023-05-18 15:58:33 +0300

    Update task-1

--- js/task-01.js
+++ js/task-01.js
@@ -2,11 +2,6 @@ const categoriesItemArrEl = document.querySelectorAll("#categories .item");
 console.log("Number of categories:", categoriesItemArrEl.length);
 
 categoriesItemArrEl.forEach((categoryItem) => {
-  const categoryTitle = categoryItem.firstElementChild;
-  console.log("Category: ", categoryTitle.textContent);
-  const categoryItems = categoryItem.lastElementChild.children;
-  console.log("Elements: ", categoryItems.length);
+  console.log("Category: ", categoryItem.firstElementChild.textContent);
+  console.log("Elements: ", categoryItem.lastElementChild.children.length);
 });
-
-// const categoryTitle = categoryItem.querySelector("h2");
-// const categoryItems = categoryItem.querySelectorAll("li");
